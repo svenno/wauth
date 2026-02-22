666c16b0b39f95425aa3ab4b41342a4236b6aa90
svenno <svenno@users.noreply.github.com>
2026-02-22 23:19:31 +0100

    Add timestamp and IP logging functionality to auth.c

--- auth.c
+++ auth.c
@@ -1,39 +1,101 @@
-// Improved logging functionality
-
 #include <stdio.h>
-#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include <shadow.h>
+#include <crypt.h>
+#include <security/pam_appl.h>
+#include <security/pam_modules.h>
+#include <syslog.h>
+#include <security/pam_ext.h>
+#include <time.h>
 
-#define ENABLE_TIMESTAMPS 1
+#define ENABLE_TIMESTAMPS 0
 #define ENABLE_IP_LOGGING 1
 
-void log_message(const char *level, const char *message, const char *remote_ip) {
-    char log_buffer[256];
-    time_t now;
-    struct tm *tm_info;
+#define LOG_MESSAGE(level, pamh, message, ...) \
+\tpam_syslog(pamh, level, "%s" message "%s", timeString, ##__VA_ARGS__, ipString)
 
-    if (ENABLE_TIMESTAMPS) {
-        time(&now);
-        tm_info = localtime(&now);
-        strftime(log_buffer, sizeof(log_buffer), "[%Y-%m-%d %H:%M:%S] ", tm_info);
-    } else {
-        log_buffer[0] = '\0'; // No timestamp
-    }
-
-    strcat(log_buffer, level);
-    strcat(log_buffer, ": ");
-    strcat(log_buffer, message);
-
-    if (ENABLE_IP_LOGGING && remote_ip != NULL) {
-        strcat(log_buffer, " (IP=");
-        strcat(log_buffer, remote_ip);
-        strcat(log_buffer, ")");
-    }
-
-    printf("%s\n", log_buffer);
+PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
+	/* printf ("setcred  %d %d\n", argc, flags); */
+	return PAM_SUCCESS;
 }
 
-int main() {
-    log_message("INFO", "User logged in", "192.168.1.1");  // Example log
-    return 0;
+PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
+	const char* pUsername;
+	char wUsername[128];
+	char* wPassword;
+	int pwTest = -1;
+	char c = 0; int i = 0;
+
+	char timeString[80] = "";
+	char ipString[64] = "";
+
+	#if ENABLE_TIMESTAMPS
+	time_t now = time(NULL);
+	struct tm *timeinfo = localtime(&now);
+	strftime(timeString, sizeof(timeString), "[%Y-%m-%d %H:%M:%S] ", timeinfo);
+	#endif
+
+	#if ENABLE_IP_LOGGING
+	const char *ipAddr = pam_getenv(pamh, "REMOTE_ADDR");
+	if (!ipAddr) {
+		ipAddr = "unknown";
+	}
+	snprintf(ipString, sizeof(ipString), " (IP=%s)", ipAddr);
+	#endif
+
+	if (PAM_SUCCESS != pam_get_user(pamh, &pUsername, NULL)) {
+	  return PAM_INCOMPLETE;
+	}
+
+	if (argc == 1) {
+	  /* first argument is a username, we use this as default */
+	  sscanf(argv[0], "%s", wUsername);
+	  printf("Withness login for %s\n", wUsername);
+	} else {
+	  printf("Whithness login: ");
+	  scanf("%s", wUsername);
+	}
+
+	if (wUsername == NULL || wUsername[0] == '-' || wUsername[0] == '+') {
+	  /* unix does not allow this as well  */
+	  LOG_MESSAGE(LOG_ERR, pamh, "bad username [%s]", wUsername);
+	  return PAM_USER_UNKNOWN;
+	}
+	wPassword = getpass("Whitness password: ");
+
+	if (0 == strcmp(wUsername, pUsername)) {
+	  printf("Whitness can not be the same user!\n");
+	  LOG_MESSAGE(LOG_CRIT, pamh, "auth whitness and login are identical [%s - %s]", pUsername, wUsername);
+	  /* return PAM_AUTH_ERR; */
+	}
+
+	struct passwd* passwdEntry = getpwnam( wUsername );
+	if ( !passwdEntry ) {
+		printf( "User '%s' doesn't exist\n", wUsername );
+		LOG_MESSAGE(LOG_CRIT, pamh, "auth could not identify username %s", wUsername);
+		return PAM_USER_UNKNOWN;
+	}
+
+	if ( 0 != strcmp( passwdEntry->pw_passwd, "x" ) ) {
+	   /* x -> we try with shadow file  */
+	   pwTest = strcmp( passwdEntry->pw_passwd, crypt( wPassword, passwdEntry->pw_passwd ) );
+	} else {
+	  struct spwd* shadowEntry = getspnam( wUsername );
+	  if ( !shadowEntry ) {
+		LOG_MESSAGE(LOG_CRIT, pamh, "auth can not read shadow entry for %s", wUsername);
+		return PAM_AUTH_ERR;
+	  }
+
+	  pwTest = strcmp( shadowEntry->sp_pwdp, crypt( wPassword, shadowEntry->sp_pwdp ) );
+	}   
+
+	if (pwTest == 0) {
+	  return PAM_SUCCESS;
+	}
+
+	return PAM_AUTH_ERR;
 }
